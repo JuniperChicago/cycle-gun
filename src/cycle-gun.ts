828d2a4d27eff9bc0019f57608c085cd576aca3b
Robin Schulemann <rschulemann@gmail.com>
2017-02-15 10:19:50 -0600

    initial commit

---
+++ src/cycle-gun.ts
@@ -0,0 +1,51 @@
+
+import xs from 'xstream';
+import * as Gun from '../node_modules/gun/gun.js';
+import dropRepeats from 'xstream/extra/dropRepeats';
+import * as equal from 'deep-equal';
+
+export function makeGunDriver(url) {
+
+    // TODO: multiple peer handling?
+
+    const gun = Gun(url);
+
+    function get(gunFn) {
+
+        if (typeof gunFn !== 'function') throw 'Function Required to get gun stream';
+
+        const loc = gunFn(gun);
+        let eventListener = null;
+
+        return xs.create({
+            start: (listener) => {
+                eventListener = (event) => {
+                    return listener.next(event);
+                };
+                loc.on(eventListener);
+            },
+            stop: () => {
+                // socket.removeEventListener(typeKey, this.eventListener);
+            }
+        })
+        .compose(dropRepeats(equal));
+    };
+
+    function processTransform(inputFunction){
+        return inputFunction(gun);
+    }
+    
+    return (event$) => {
+        event$.addListener({
+            next: (transform) => {
+                // TODO: Error handling
+                return processTransform(transform);
+            }
+        });
+
+        return {
+            get
+        };
+    };
+
+} 
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+
+export {makeGunDriver} from './cycle-gun';
+
+
+
